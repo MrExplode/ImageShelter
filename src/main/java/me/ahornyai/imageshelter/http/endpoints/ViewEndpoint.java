df44999e05d55a15497e910be3a36188a3851d9b
Hornyai Alex <ahornyai@gmail.com>
2020-09-13 11:03:20 +0200

    ViewEndpoint

--- src/main/java/me/ahornyai/imageshelter/http/endpoints/ViewEndpoint.java
+++ src/main/java/me/ahornyai/imageshelter/http/endpoints/ViewEndpoint.java
@@ -2,13 +2,42 @@ package me.ahornyai.imageshelter.http.endpoints;
 
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
+import me.ahornyai.imageshelter.http.responses.ErrorResponse;
 import org.jetbrains.annotations.NotNull;
 
-public class ViewEndpoint implements Handler {
-    @Override
-    public void handle(@NotNull Context ctx) {
-        //TODO: path traversal protection
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.regex.Pattern;
 
-        ctx.result("view endpoint params: " + ctx.pathParam("file") + " " + ctx.pathParam("key"));
+public class ViewEndpoint implements Handler {
+    private static final Pattern PATH_TRAVERSAL = Pattern.compile("[^0-9A-Za-z.]");
+
+    @Override
+    public void handle(@NotNull Context ctx) throws FileNotFoundException {
+        String fileParam = ctx.pathParam("file");
+
+        if (PATH_TRAVERSAL.matcher(fileParam).find()) {
+            ctx.redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+            return;
+        }
+
+        File file = new File("uploads" + File.separator + fileParam);
+
+        if (!file.exists()) {
+            ctx.json(new ErrorResponse("FILE_DOES_NOT_EXIST","This file does not exist."));
+
+            return;
+        }
+
+        if (!file.canRead()) {
+            ctx.json(new ErrorResponse("READ_PERMISSION","I don't have read permission to uploads/" + fileParam + " file."));
+
+            return;
+        }
+
+        //TODO: decrypt, compress
+
+        ctx.result(new FileInputStream(file));
     }
 }
